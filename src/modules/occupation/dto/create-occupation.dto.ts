53bf50ea1a7a2a51ef8d5713ce3fd1c0d83cdcb9
joaobuga35 <joaobuga35@gmail.com>
2023-08-29 23:01:02 -0300

    feat: create dto and entity, with class-validators

--- src/modules/occupation/dto/create-occupation.dto.ts
+++ src/modules/occupation/dto/create-occupation.dto.ts
@@ -1,1 +1,19 @@
-export class CreateOccupationDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateOccupationDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  office: string;
+
+  @IsOptional()
+  @IsString()
+  linkedin?: string;
+
+  @IsOptional()
+  @IsString()
+  discord?: string;
+}
--- src/modules/occupation/entities/occupation.entity.ts
+++ src/modules/occupation/entities/occupation.entity.ts
@@ -1,1 +1,9 @@
-export class Occupation {}
+export class Occupation {
+  readonly id: string;
+  name: string;
+  office: string;
+  linkedin?: string;
+  discord?: string;
+  readonly created_at: Date;
+  readonly updated_at: Date;
+}
